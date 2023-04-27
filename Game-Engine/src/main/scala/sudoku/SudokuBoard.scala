657ee58b991062f6740d46517026d5acd5d3944d
Mariam Hossam <m.hossam2551@gmail.com>
2023-04-27 23:41:06 +0200

    Make initialBoard val not var

--- Game-Engine/src/main/scala/sudoku/SudokuBoard.scala
+++ Game-Engine/src/main/scala/sudoku/SudokuBoard.scala
@@ -3,22 +3,22 @@ package sudoku
 class SudokuBoard {
   val size = 9
   var board: Array[Array[Int]] = Array.ofDim[Int](size, size)
-  var intialBoard: Array[Array[Int]] = Array.ofDim[Int](size, size)
+  val initialBoard: Array[Array[Int]] = Array.ofDim[Int](size, size)
   //initial
-  SudokuGenerator.getSudoku(intialBoard)
-  board=intialBoard.map(_.clone)
+  SudokuGenerator.getSudoku(initialBoard)
+  board = initialBoard.map(_.clone)
 
   def validPlace(x: Int, y: Int): Boolean = {
-    println("x "+x+" y "+y)
+    println("x " + x + " y " + y)
     x >= 0 && x <= 8 && y >= 0 && y <= 8
   }
 
   def validDelete(x: Int, y: Int): Boolean = {
-    println("In validDelete "+"x " + x + " y " + y)
-    intialBoard(x)(y)==0
+    println("In validDelete " + "x " + x + " y " + y)
+    initialBoard(x)(y) == 0
   }
 
-  def addMove(x: Int, y: Int, z:Int): Array[Array[Int]] = {
+  def addMove(x: Int, y: Int, z: Int): Array[Array[Int]] = {
     board(x)(y) = z
     println("In add move")
     board
