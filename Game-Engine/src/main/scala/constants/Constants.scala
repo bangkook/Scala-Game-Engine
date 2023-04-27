90ca7bec9df9f9f80a0ce53e3018530cec6db0e1
Mariam Hossam <m.hossam2551@gmail.com>
2023-04-27 23:39:34 +0200

    Add games' names as constants

---
+++ Game-Engine/src/main/scala/constants/Constants.scala
@@ -0,0 +1,10 @@
+package constants
+
+object Constants {
+  val chess: String = "Chess"
+  val tic_tac_toe: String = "Tic-Tac-Toe"
+  val checkers: String = "Checkers"
+  val queens: String = "8-Queens"
+  val sudoku: String = "Sudoku"
+  val connect: String = "Connect-4"
+}
