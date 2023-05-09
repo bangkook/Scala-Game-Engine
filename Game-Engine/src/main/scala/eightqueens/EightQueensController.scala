eddeb3eb74dbb8d623db3e348f479b7813b2ac7b
emansamir2 <51717395+emansamir2@users.noreply.github.com>
2023-05-09 19:01:11 +0300

    Update EightQueensController.scala

--- Game-Engine/src/main/scala/eightqueens/EightQueensController.scala
+++ Game-Engine/src/main/scala/eightqueens/EightQueensController.scala
@@ -8,45 +8,55 @@ import scala.annotation.tailrec
 object EightQueensController {
 
   def control(state: GameState, move: List[String]): GameState = { //check the input // a01
-    if(move.head.length != 3)
+    if(move.head.length != 2)
       return state
 
     val size = state.board.length
-    val queen: Int = move.head(0) - 'a'
-    val from: Int = size - (move.head(1) - '1') - 2
-    val to = size - (move.head(2)- '1') - 2
-    println(s"Queen: $queen, From: $from, To: $to")
+    val to: Int = size - (move.head(0) - '1') - 2
+    val queen: Int = move.head(1) - 'a'
 
     // If out of bounds or cell is empty, return false
-    if (!insideBoard(from, queen, state.board) || !insideBoard(to, queen, state.board) || state.board(from)(queen) == null) {
+    if (!insideBoard(to, queen, state.board) ) {
       println(1)
       return state
     }
-    if (!validMove(state.board, (from, queen), (to, queen))) {
+
+    if(state.board(to)(queen) != null){ //piece
+      val newBoard = deleteMove((to, queen), state.board);
+      return GameState(!state.player, newBoard)
+    }
+
+    if (!validMove(state.board,  (to, queen))) {
       println(5)
       return state
     }
-    val newBoard = addMove((from, queen), (to, queen), state.board)
+    val newBoard = addMove( (to, queen), state.board);
     GameState(!state.player, newBoard)
   }
-
-  def addMove(from: (Int, Int), to: (Int, Int), board: Array[Array[GamePiece]] ): Array[Array[GamePiece]] = {
-    val name = board(from._1)(from._2).name
-    print(name)
+  def deleteMove(to: (Int, Int), board: Array[Array[GamePiece]]): Array[Array[GamePiece]] = {
     val newBoard: Array[Array[GamePiece]] = Array.tabulate(board.length, board(0).length)((x, y) =>
-      if (x == to._1 && y == to._2) GamePiece(name, null)
-      else if (x == from._1 && y == from._2) null
+      if (x == to._1 && y == to._2) null
+      else if (x == 8 && y == to._2) GamePiece("queens", null)
+      else board(x)(y)
+    )
+    newBoard
+  }
+  def addMove( to: (Int, Int), board: Array[Array[GamePiece]] ): Array[Array[GamePiece]] = {
+    val newBoard: Array[Array[GamePiece]] = Array.tabulate(board.length, board(0).length)((x, y) =>
+      if (x == to._1 && y == to._2) GamePiece("queens", null)
+      else if (x == 8 && y == to._2) null
       else board(x)(y)
     )
     newBoard
   }
 
-  private def validMove(board: Array[Array[GamePiece]], from: (Int, Int), to: (Int, Int)): Boolean = {
-    if (to._1 == 8) {
-      return true;
-    }
+  private def validMove(board: Array[Array[GamePiece]],  to: (Int, Int)): Boolean = {
+
     val x2 = to._1
     val y2 = to._2
+    if(board(8)(y2) == null){
+      return false
+    }
     for (i <- 0 to 7) { //check row
       if (board(x2)(i) != null) {
         return false;
@@ -79,6 +89,3 @@ object EightQueensController {
     return checkUpperLeft(x2, y2) && checkUpperRight(x2, y2) && checkLowerRight(x2, y2) && checkLowerLeft(x2, y2)
   }
 }
-
-
-
